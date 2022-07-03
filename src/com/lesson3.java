f504136d93fbb920e11557a3f74dacf4fce16661
Ivan <lauriantus@gmail.com>
2022-07-03 22:03:52 +0300

    new lesson. repeat

---
+++ src/com/lesson3.java
@@ -0,0 +1,16 @@
+package com;
+
+public class lesson3 {
+    public static void main(String[] args) {
+        byte b = 127;
+        short s = 10000;
+        char c = 49;
+        int i = 589695;
+        long l = 6546421L;
+        float f = 21.75F;
+        double d = 121.3;
+        boolean bool = true;
+
+        String str = "Hello!";
+    }
+}
